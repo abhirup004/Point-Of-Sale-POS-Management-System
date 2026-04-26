9c280b2eef4fa9d65c722ce4dda174483a456b17
camelia0011 <cameliachowdhury12@gmail.com>
2026-04-26 17:09:27 +0530

    First Commit from Camelia

--- src/main/java/com/jbs/posbe/PosbeApplication.java
+++ src/main/java/com/jbs/posbe/PosbeApplication.java
@@ -13,6 +13,7 @@ public class PosbeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PosbeApplication.class, args);
 		logger.info("PosbeApplication started successfully...");
+		logger.info("Camelia Chowdhury - 2024-06-01");
 	}
 
 }
