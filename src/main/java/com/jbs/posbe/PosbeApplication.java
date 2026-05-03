2aa7b43a158124e3a4309ee7ccb0fd27d63d94ca
Agni-glitch <agniroybar@gmail.com>
2026-05-03 14:48:47 +0530

    first commit from Agni-glitch

--- src/main/java/com/jbs/posbe/PosbeApplication.java
+++ src/main/java/com/jbs/posbe/PosbeApplication.java
@@ -13,6 +13,7 @@ public class PosbeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PosbeApplication.class, args);
 		logger.info("PosbeApplication started successfully...");
+		logger.info("This is from Agni-Glitch branch...");
 	}
 
 }
